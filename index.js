5260693bd9b5eca877bce124d781775cd0714a38
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-05-22 20:09:24 -0400

    commit tree 4c25bc21d983e47bf987cc38cb2a49fc16eaf744

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    let riders = drivers.filter(drivers => drivers == name || drivers == name.toLowerCase())
+    return riders
+ 
+}
+function fuzzyMatch(drivers, letter) {
+    let riders = drivers.filter(drivers => drivers.slice(0, letter.length) === letter)
+    return riders
+}
+function matchName(drivers, string){
+    let riders = drivers.filter(drivers => drivers.name === string)
+    return riders
+
+}
